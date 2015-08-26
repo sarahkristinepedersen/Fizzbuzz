b7410216ff5e15d17fa4c7ecf72a4201a5426d9f
Sarah Kristine Pedersen <mail@sarahkp.dk>
2015-08-26 12:00:11 +0100

    test added

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,17 @@
-require 'fizzbuzz./lib/fizzbuzz'
+require './lib/fizzbuzz'
 
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+
+  it 'returns “buzz" when passed 5' do
+   expect(fizzbuzz(5)).to eq 'buzz'
+ end
+
+#fizzbuzz = 15
+
+
 end
+
+
